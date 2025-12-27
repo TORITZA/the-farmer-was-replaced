45e0777a3ad78dee338e2a75c038c9842b70e265
TORITZA <terratreehive@gmail.com>
2025-12-27 09:29:57 -0500

    added poly_tree() function

--- planting.py
+++ planting.py
@@ -81,22 +81,6 @@ def weird_sub(p: dict) -> None:
 			till()
 		plant(p['sunflwr'])
 
-def tree_planter(p):
-	for i in range(get_world_size()):
-		if get_pos_x() % 2 == 0 and get_pos_y() % 2 == 0 or get_pos_x() % 2 != 0 and get_pos_y() % 2 != 0:
-			if get_ground_type() == Grounds.Grassland:
-				till()
-			plant(p['tree'])
-			use_item(Items.Water)
-		move(North)
-
-def poly_tree(p):
-	for i in range(get_world_size()):
-		if get_entity_type() == p['tree']:
-			while get_companion() != Entities.Grass:
-				plant(p['tree'])
-		move(North)
-
 
 
 
--- tree.py
+++ tree.py
@@ -1,2 +1,22 @@
 # Tree harvester utilizing polyculture:
 
+def poly_tree(p):
+	'''
+    W.I.P: TO BE FURTHER OPTIMIZED 
+    
+    :param p: dict of crop entities 
+    '''
+	for i in range(get_world_size()):
+		if get_pos_x() % 2 == 0 and get_pos_y() % 2 == 0 or get_pos_x() % 2 != 0 and get_pos_y() % 2 != 0:
+			till()
+			plant(p['tree'])
+			companion, (x, y) = get_companion()
+			while companion != Entities.Grass and (x + y) % 2 != 1:
+				harvest()
+				plant(p['tree'])
+				use_item(Items.Water)
+				companion, (x, y) = get_companion()
+		move(North)
+		if can_harvest():
+			harvest()
+			plant(Entities.Grass)
