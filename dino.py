3b443e13aa68b2f7d21c2e35f6e0685e59afd738
TORITZA <terratreehive@gmail.com>
2025-12-29 08:15:33 -0500

    renamed snake.py to dino.py and added stubbed-out functions

---
+++ dino.py
@@ -0,0 +1,11 @@
+# start 0,0 -> traverse entire farm; loop -> collect apples 
+# "direction" dict (turn left, turn right)
+
+def turn_left(dir):
+    pass
+
+def turn_right(dir):
+    pass
+
+def snake(dir):
+    pass
