bd51a1232abda054a87915d7ca62b355a2ec0309
TORITZA <terratreehive@gmail.com>
2025-12-30 07:58:32 -0500

    implemented untested snake() func in dino.py

--- dino.py
+++ dino.py
@@ -1,11 +1,32 @@
 # start 0,0 -> traverse entire farm; loop -> collect apples 
 # "direction" dict (turn left, turn right)
 
-def turn_left(dir):
-    pass
+# directions = ("North", "East", "South", "West")
+# position = 0
 
-def turn_right(dir):
-    pass
+def turn_left():
+    move(South)
+    for i in range(get_world_size() - 1):
+        move(East)
 
-def snake(dir):
-    pass
+def turn_right():
+    move(South)
+    for i in range(get_world_size() - 1):
+        move(West)
+
+def snake():
+    x, y = get_pos_x(), get_pos_y()
+    
+    if move() is False:
+        print("ERROR")
+    
+    if x == 0 and y == 0:
+        for i in range(get_world_size()):
+            move(North)
+        for i in range(get_world_size()):
+            move(East)
+    elif x == get_world_size() - 1:
+        turn_right()
+    elif x == 1:
+        turn_left() 
+        
