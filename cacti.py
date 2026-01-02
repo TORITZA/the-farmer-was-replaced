18689ce6600bdb8705b065a7386bc8d1a51bc1c7
TORITZA <terratreehive@gmail.com>
2026-01-02 11:00:38 -0500

    implemented untested sort_cactus (bubble sort) in cacti.py

--- cacti.py
+++ cacti.py
@@ -5,19 +5,12 @@ Module to maximize cacti harvest
 # GLOBALS: 
 X, Y = get_pos_x(), get_pos_y()
 
-def plant_cacti(p: dict) -> None:
-    '''
-    Coats the farm in cacti crops as a primer for harvest 
-    
-    :param p: dict of crop entities 
-    '''
+def plant_cacti(p):
     for i in range(get_world_size()):
-        if get_ground_type() == Grounds.Grassland:
-            till()
+        till()
         plant(p['cactus'])
-        move(North)
-    move(East)
-    
+
+
 def sort_cacti(p: dict) -> None: 
     '''
     Reconfigures the arrangement of the cacti to be in sorted order
@@ -25,21 +18,17 @@ def sort_cacti(p: dict) -> None:
     
     :param p: dict of crop entities
     '''
-    for i in range(get_world_size()):
-        if can_move(East) and measure() < measure(East):
-            swap(East)
-        if can_move(North) and measure() > measure(North):
-            swap(North)
-        move(North)
-    move(East)
+    plant_cacti(p)
+    is_sorted = False
 
-def harvest_cacti(p: dict) -> None:
-    '''
-    Harvests all cacti 
+# Continue looping as long as the list is not marked as sorted:
+    while not is_sorted:
+        # Assume the list is sorted at the start of the pass (loop condition changes): 
+        is_sorted = True
+        for i in range(get_world_size() - 1):
+            if measure() > measure(East):
+                swap(East)
+                is_sorted = False
+            move(North)
     
-    :param p: dict of crop entities 
-    '''
-    for i in range(get_world_size()):
-        harvest()
-        move(North)
-    move(East)
+    
