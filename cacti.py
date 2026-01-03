cb6d5b88fa39353b209ed6267236e9ad0683c533
TORITZA <terratreehive@gmail.com>
2026-01-03 10:36:35 -0500

    completed single drone cacti farmer in cacti.py

--- cacti.py
+++ cacti.py
@@ -2,33 +2,54 @@
 Module to maximize cacti harvest
 '''
 
-# GLOBALS: 
-X, Y = get_pos_x(), get_pos_y()
-
 def plant_cacti(p):
-    for i in range(get_world_size()):
-        till()
-        plant(p['cactus'])
+	for i in range(get_world_size()):
+		if get_ground_type() == Grounds.Grassland:	
+			till()
+		plant(p['cactus'])
+		use_item(Items.WAter)
+		move(East)
 
-
-def sort_cacti(p: dict) -> None: 
-    '''
-    Reconfigures the arrangement of the cacti to be in sorted order
+def v_sort_cacti(p): 
+	'''
+    Vertically re-configures the arrangement of the cacti to be in sorted order
     (Bubble sort)  
     
     :param p: dict of crop entities
     '''
-    plant_cacti(p)
-    is_sorted = False
+	plant_cacti(p)
+	is_sorted = False
+	n = get_world_size()
 
 # Continue looping as long as the list is not marked as sorted:
-    while not is_sorted:
-        # Assume the list is sorted at the start of the pass (loop condition changes): 
-        is_sorted = True
-        for i in range(get_world_size() - 1):
-            if measure() > measure(East):
-                swap(East)
-                is_sorted = False
-            move(North)
+	while not is_sorted:
+		# Assume the list is sorted at the start of the pass (loop condition changes): 
+		is_sorted = True
+		for i in range(n - 1):
+			if measure() > measure(East):
+				swap(East)
+				is_sorted = False
+			move(East)
+	if is_sorted:
+		move(North)
+		
+def h_sort_cacti(p):
+	'''
+    Horizontally re-configures the arrangement of the cacti to be in sorted order
+    (Bubble sort)  
     
-    
+    :param p: dict of crop entities
+    '''
+	is_sorted = False
+
+	while not is_sorted:
+		# Assume the list is sorted at the start of the pass (loop condition changes): 
+		is_sorted = True
+		for i in range(get_world_size() - 1):
+			if measure() > measure(North):
+				swap(North)
+				is_sorted = False
+			move(North)
+	if is_sorted:
+		move(East)
+		
