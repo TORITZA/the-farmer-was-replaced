70fe85db2b2b039a291231c52cfab5be37120f5d
TORITZA <terratreehive@gmail.com>
2026-01-11 12:08:48 -0500

    implemented working sun-power MegaFarm harvester in sunflower.py

--- sunflower.py
+++ sunflower.py
@@ -4,7 +4,8 @@ Docstring for sunflower
 
 def plant_sunflowers_first():
     for i in range(get_world_size()):
-        till()
+        if get_ground_type() == Grounds.Grassland:
+            till()
         plant(Entities.Sunflower)
         use_item(Items.Water)
         move(East)
@@ -12,10 +13,24 @@ def plant_sunflowers_first():
 def power_plant():
     flowers = get_world_size()
     while flowers > 0:
-        for i in range(get_world_size()):
-            for petals in range(15, 6, -1):
+        for petals in range(15, 6, -1):
+            for i in range(get_world_size()):
                 if measure() == petals:
                     harvest()
                     flowers -= 1
-            move(East)
+                move(East)
+
+# SUN-POWER MEGAFARMER:
+"""
+while True:
+    for i in range(get_world_size()):
+        move(North)
+        spawn_drone(plant_sunflowers_first)
+    plant_sunflowers_first()
+    for i in range(get_world_size()):
+        move(North)
+        spawn_drone(power_plant)
+    power_plant()
+"""
+
 
