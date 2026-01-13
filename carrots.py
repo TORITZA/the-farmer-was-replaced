84002fe1f9a1d95281af4a6032485a5b19cf6cb4
TORITZA <terratreehive@gmail.com>
2026-01-13 12:58:44 -0500

    implemented working MegaFarm carrot harvester

--- carrots.py
+++ carrots.py
@@ -24,3 +24,36 @@ def poly_carrot(p):
         if can_harvest():
             harvest()
             plant(Entities.Grass)
+
+
+def mega_carrot(p):
+    '''
+    Docstring for mega_carrot
+    
+    :param p: Description
+    '''
+    while True:
+        for i in range(get_world_size()):
+            if get_pos_x() % 2 == 0 and get_pos_y() % 2 == 0 or get_pos_x() % 2 != 0 and get_pos_y() % 2 != 0:
+                if get_ground_type() == Grounds.Grassland:
+                    till()
+                    plant(Entities.Carrot)
+                    companion, (x, y) = get_companion()
+                    while companion != Entities.Grass and (x + y) % 2 != 1:
+                        harvest()
+                        plant(Entities.Carrot)
+                        use_item(Items.Water)
+                        companion, (x, y) = get_companion()
+                move(North)
+                if can_harvest():
+                    harvest()
+
+
+# CARROT MEGAFARMER:
+"""
+for i in range(get_world_size()):
+	move(East)
+	spawn_drone(mega_carrot)
+mega_carrot()
+
+"""
