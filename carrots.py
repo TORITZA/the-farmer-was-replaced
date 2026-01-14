d5555c9762cf5e6e9a4192cfe58129eef234cc77
TORITZA <terratreehive@gmail.com>
2026-01-14 15:01:26 -0500

    created tree.py

--- carrots.py
+++ carrots.py
@@ -37,13 +37,13 @@ def mega_carrot(p):
             if get_pos_x() % 2 == 0 and get_pos_y() % 2 == 0 or get_pos_x() % 2 != 0 and get_pos_y() % 2 != 0:
                 if get_ground_type() == Grounds.Grassland:
                     till()
+                plant(Entities.Carrot)
+                companion, (x, y) = get_companion()
+                while companion != Entities.Grass and (x + y) % 2 != 1:
+                    harvest()
                     plant(Entities.Carrot)
+                    use_item(Items.Water)
                     companion, (x, y) = get_companion()
-                    while companion != Entities.Grass and (x + y) % 2 != 1:
-                        harvest()
-                        plant(Entities.Carrot)
-                        use_item(Items.Water)
-                        companion, (x, y) = get_companion()
                 move(North)
                 if can_harvest():
                     harvest()
--- tree.py
+++ tree.py
@@ -8,7 +8,8 @@ def poly_tree(p):
     '''
 	for i in range(get_world_size()):
 		if get_pos_x() % 2 == 0 and get_pos_y() % 2 == 0 or get_pos_x() % 2 != 0 and get_pos_y() % 2 != 0:
-			till()
+			if get_ground_type() == Grounds.Grassland:
+				till()
 			plant(p['tree'])
 			companion, (x, y) = get_companion()
 			while companion != Entities.Grass and (x + y) % 2 != 1:
@@ -20,3 +21,26 @@ def poly_tree(p):
 		if can_harvest():
 			harvest()
 			plant(Entities.Grass)
+
+def mega_tree():
+	'''
+	Docstring for mega_tree
+	'''
+	while True: 
+		for i in range(get_world_size()):
+			if get_pos_x() % 2 == 0 and get_pos_y() % 2 == 0 or get_pos_x() % 2 != 0 and get_pos_y() % 2 != 0:
+				if get_ground_type() == Grounds.Grassland:
+					till()
+			plant(Entities.Tree)
+			companion, (x, y) = get_companion()
+			while companion != Entities.Grass and (x + y) % 2 != 1:
+				harvest()
+				plant(Entities.Tree)
+				use_item(Items.Water)
+				companion, (x, y) = get_companion()
+		move(North)
+		if can_harvest():
+			harvest()
+			plant(Entities.Grass)
+
+
